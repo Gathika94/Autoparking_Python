7d74d910369dfe3f509485183e4e70ec0e1b92c7
Gathika94 <gathika.14@cse.mrt.ac.lk>
2019-01-08 21:36:48 +0530

    api for occupance detection eposed

--- predictor/executor.py
+++ predictor/executor.py
@@ -1,17 +1,67 @@
 
 import forward
 import numpy as np;
+from flask import Flask, request
+from flask_restful import Resource, Api
+import json
+from sqlalchemy import create_engine
+import subprocess
+import os
 
-out =forward.forward_all("/media/gathika/MainDisk/entgra_repos/deep-parking/mAlexNet-on-Combined_CNRParkAB_Ext_train-val-PKLot_val/deploy.prototxt", "/media/gathika/MainDisk/entgra_repos/deep-parking/mAlexNet-on-Combined_CNRParkAB_Ext_train-val-PKLot_val/snapshot_iter_6275.caffemodel",
-                    "images.txt");
-np.save('predictions.npy', out)
-newArray = np.load("predictions.npy")
 
 
-for x in range(0,23,1):
-    availability = (newArray[x,0])
-    if(availability<1.0*(10**(-25))):
-        print ("slot"+str(x+1)+": "+str(0))
-    else:
-        print ("slot" +str(x+1) + ": " + str(1))
+app = Flask(__name__)
+api = Api(app)
 
+def methodRunner():
+    out = forward.forward_all(
+        "/media/gathika/MainDisk/entgra_repos/deep-parking/mAlexNet-on-Combined_CNRParkAB_Ext_train-val-PKLot_val/deploy.prototxt",
+        "/media/gathika/MainDisk/entgra_repos/deep-parking/mAlexNet-on-Combined_CNRParkAB_Ext_train-val-PKLot_val/snapshot_iter_6275.caffemodel",
+        "images.txt")
+    print ("received")
+    np.save('predictions.npy', out)
+    newArray = np.load("predictions.npy")
+
+    output = [{}]
+
+    for x in range(0, 23, 1):
+        availability = (newArray[x, 0])
+        if (availability < 1.0 * (10 ** (-25))):
+            print ("slot" + str(x + 1) + ": " + str(0))
+            output[0]["slot" + str(x + 1)] = 0
+        else:
+            print ("slot" + str(x + 1) + ": " + str(1))
+            output[0]["slot" + str(x + 1)] = 1
+    return output
+
+
+def scriptRunner():
+    cmd = 'python forward.py /media/gathika/MainDisk/entgra_repos/deep-parking/mAlexNet-on-Combined_CNRParkAB_Ext_train-val-PKLot_val/deploy.prototxt /media/gathika/MainDisk/entgra_repos/deep-parking/mAlexNet-on-Combined_CNRParkAB_Ext_train-val-PKLot_val/snapshot_iter_6275.caffemodel images.txt predictions.npy'
+    #p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    os.system(cmd)
+    newArray = np.load("predictions.npy")
+
+    output = [{}]
+
+    for x in range(0, 23, 1):
+        availability = (newArray[x, 0])
+        if (availability < 1.0 * (10 ** (-25))):
+            print ("slot" + str(x + 1) + ": " + str(0))
+            output[0]["slot" + str(x + 1)] = 0
+        else:
+            print ("slot" + str(x + 1) + ": " + str(1))
+            output[0]["slot" + str(x + 1)] = 1
+    return output
+    
+
+class APIOutput(Resource):
+    def get(self):
+        return scriptRunner()
+
+
+api.add_resource(APIOutput, '/output')
+
+
+
+if __name__ == '__main__':
+    app.run(port='5004')

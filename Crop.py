9bbbfc42eb32af5642fed69132f53b6b0625be00
Gathika94 <gathika.14@cse.mrt.ac.lk>
2019-01-10 18:01:12 +0530

    ml microservice with necessary parameters

--- Crop.py
+++ Crop.py
@@ -1,12 +1,30 @@
 import cv2
 
 
-img = cv2.imread("/media/gathika/MainDisk/entgra_repos/AutoParking/images/original/park_side4.jpg",1)
-print (img.shape)
+#img = cv2.imread("/media/gathika/MainDisk/entgra_repos/asas/autoparking/images/camera1/park_side4.jpg",1)
+#coordinates = [[436,592,256,476], [309,420,214,475], [204,279,250,484]]
+#folderPath = "/media/gathika/MainDisk/entgra_repos/asas/autoparking/images/camera1_slots/";
+#print (img.shape)
 
-cropped1 = img[436:592,256:476]
-cropped2 = img[309:420,214:475]
-cropped3 = img[204:279,250:484]
-cv2.imwrite("/media/gathika/MainDisk/entgra_repos/AutoParking/images/cropped/park_cropped1.png",cropped1)
-cv2.imwrite("/media/gathika/MainDisk/entgra_repos/AutoParking/images/cropped/park_cropped2.png",cropped2)
-cv2.imwrite("/media/gathika/MainDisk/entgra_repos/AutoParking/images/cropped/park_cropped3.png",cropped3)
+
+def cropper(coordinates, image):
+
+    slots = len(coordinates);
+    count = 1;
+    for i in range(0,slots,1):
+        x1 = coordinates[i][0]
+        x2 = coordinates[i][1]
+        y1 = coordinates[i][2]
+        y2 = coordinates[i][3]
+        croppedImg = image[x1:x2,y1:y2]
+        #cropped2 = img[309:420,214:475]
+        #cropped3 = img[204:279,250:484]
+        folderPath = "/media/gathika/MainDisk/entgra_repos/asas/autoparking/images/camera1_slots/"
+        filePath = folderPath+"slot"+str(i+1)+"."+"png"
+        print filePath
+       # filePath = "/media/gathika/MainDisk/entgra_repos/AutoParking/images/cropped/park_cropped2.png"
+        cv2.imwrite(str(filePath),croppedImg)
+        #cv2.imwrite("/media/gathika/MainDisk/entgra_repos/AutoParking/images/cropped/park_cropped2.png",croppedImg)
+        #cv2.imwrite("/media/gathika/MainDisk/entgra_repos/AutoParking/images/cropped/park_cropped3.png",cropped3)
+
+#cropper(coordinates,img)
--- predictor/executor.py
+++ predictor/executor.py
@@ -1,5 +1,6 @@
 
 import forward
+import Crop
 import numpy as np;
 from flask import Flask, request
 from flask_restful import Resource, Api
@@ -7,7 +8,7 @@ import json
 from sqlalchemy import create_engine
 import subprocess
 import os
-
+import cv2
 
 
 app = Flask(__name__)
@@ -41,22 +42,48 @@ def scriptRunner():
     os.system(cmd)
     newArray = np.load("predictions.npy")
 
-    output = [{}]
+    output = {}
+    available = 0
+    occupied  = 0
 
     for x in range(0, 23, 1):
         availability = (newArray[x, 0])
-        if (availability < 1.0 * (10 ** (-25))):
+        if (availability < 4.0 * (10 ** (-24))):
             print ("slot" + str(x + 1) + ": " + str(0))
-            output[0]["slot" + str(x + 1)] = 0
+            output["slot" + str(x + 1)] = 0
+            print ("availability : "+str(availability))
+            occupied = occupied+1
         else:
             print ("slot" + str(x + 1) + ": " + str(1))
-            output[0]["slot" + str(x + 1)] = 1
-    return output
+            output["slot" + str(x + 1)] = 1
+            print ("availability : " + str(availability))
+            available=available+1
+
+    output["available"] = available
+    output["occupied"] = occupied
+    jsonOutput = json.dumps(output)
+    return jsonOutput
     
+
+
 
 class APIOutput(Resource):
     def get(self):
-        return scriptRunner()
+        storage = "/media/gathika/MainDisk/entgra_repos/asas/autoparking/images/"
+        #coordinates = [[436, 592, 256, 476], [309, 420, 214, 475], [204, 279, 250, 484]]
+        url = request.args.get('camurl')
+        print "url :" + str(url)
+        coordinates = request.args.get('coord')
+        print "coord :"+coord
+        imagePath = storage+str(url)+"/"+"park_side4.jpg"
+        image = cv2.imread(imagePath,1)
+        print "aaa"
+        Crop.cropper(coordinates,image)
+        print "bbb"
+
+
+        return {"Hello": "World"}
+        #return scriptRunner()
 
 
 api.add_resource(APIOutput, '/output')
@@ -64,4 +91,4 @@ api.add_resource(APIOutput, '/output')
 
 
 if __name__ == '__main__':
-    app.run(port='5004')
+    app.run(port='5002')
